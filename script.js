7afc847a0a35b96b050f75c8d7f416ef3f3bb966
Andrew <kolbe1129@gmail.com>
2020-10-14 18:42:11 -0400

    Completed with Review

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
 
     //starting div
     let div = document.createElement('div');
@@ -7,43 +6,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //h1-h6 - append all to div
     let h1 = document.createElement('h1');
-    h1.className = 'h1';
-    h1.className = 'h';
+    h1.className = 'h1 h';
     let text1 = document.createTextNode('This is an h1');
     h1.appendChild(text1);
     div.appendChild(h1);
 
     let pizza = document.createElement('h2');
     pizza.className = 'h2';
-    pizza.className = 'h';
+    pizza.classList.add('h');
     let text2 = document.createTextNode('This is an h2');
     pizza.appendChild(text2);
     div.appendChild(pizza);
 
     let h3 = document.createElement('h3');
-    h3.className = 'h3';
-    h3.className = 'h';
+    h3.className = 'h3 h';
     let text3 = document.createTextNode('This is an h3');
     h3.appendChild(text3);
     div.appendChild(h3);
 
     let h4 = document.createElement('h4');
     h4.className = 'h4';
-    h4.className = 'h';
+    h4.classList.add('h');
     let text4 = document.createTextNode('This is an h4');
     h4.appendChild(text4);
     div.appendChild(h4);
 
     let h5 = document.createElement('h5');
-    h5.className = 'h5';
-    h5.className = 'h';
+    h5.className = 'h5 h';
     let text5 = document.createTextNode('This is an h5');
     h5.appendChild(text5);
     div.appendChild(h5);
 
     let h6 = document.createElement('h6');
     h6.className = 'h6';
-    h6.className = 'h';
+    h6.classList.add('h');
     let text6 = document.createTextNode('This is an h6');
     h6.appendChild(text6);
     div.appendChild(h6);
@@ -60,23 +56,39 @@ document.addEventListener('DOMContentLoaded', function () {
     const headerClass = document.getElementsByClassName('h');
     for (let i = 0; i < headerClass.length; i++) {
         headerClass[i].addEventListener('dblclick', function () {
-            //console.log(headerClass[i]);
-            Math.floor(Math.random() * colorArray.length);
-        });
-        
-    }
-
-
-    //List Items - Having trouble with 1.The button being in the html 2.How to count +1 on every click
-    function list() {
-    let listItem = document.createElement('li');
-    let x = 1;
-    //x++; Nope
-    let listText = document.createTextNode('This is list item ' + x);
-    listItem.appendChild(listText);
-    document.body.appendChild(listItem);
+            headerClass[i].style.color = getRando();
+        })
     };
 
-    //document.addEventListener('click', list); //should be button.addEventListener('click', list);
-  
-})
+    // connecting button from html to js
+    const button = document.getElementsByTagName('button')[0];
+    button.addEventListener('click', list);
+
+
+    //List Items
+    let x = 0;
+    function list() {
+        let listItem = document.createElement('li');
+        x++;
+        let listText = document.createTextNode('This is list item ' + x);
+        listItem.appendChild(listText);
+        document.body.appendChild(listItem);
+
+
+        listItem.addEventListener('mouseover', function () {
+            listItem.style.color = getRando();
+        })
+
+        listItem.addEventListener('dblclick', function () {
+            document.body.removeChild(listItem);
+        })
+    };
+
+
+
+
+
+function getRando() {
+    const randomColor = Math.floor(Math.random() * colorArray.length);
+    return colorArray[randomColor];
+}
